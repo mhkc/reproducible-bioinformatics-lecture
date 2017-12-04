4cd3e0d653831b3dac5f2847c7b0b5e591af2575
Markus Johansson <markus.johansson@1928diagnostics.com>
2017-12-04 10:25:55 +0100

    Added errors to mapping.py for flake8 to catch

--- example_pipeline/pipeline/mapping.py
+++ example_pipeline/pipeline/mapping.py
@@ -1,4 +1,3 @@
-"""Functions for mapping reads and manipulating alignments."""
 import os
 
 import pandas as pd
@@ -22,10 +21,10 @@ def index_reference(ctx: ExecutionContext) -> str:
     return index_path
 
 
-def mapp_reads(ctx: ExecutionContext, indexed_ref: str) -> str:
+def mapp_reads(ctx, indexed_ref):
     """Map reads against a reference sequence using BWA."""
     # Naively remove file suffix by splitting on '.' and taking the first entry
-    sample_name = os.path.basename(ctx.sample_location[0]).split('.')[0]
+    # sample_name = os.path.basename(ctx.sample_location[0]).split('.')[0]
     reference_name = os.path.basename(ctx.reference_location).split('.')[0]
 
     # Define output file
@@ -71,8 +70,7 @@ def parse_read_cov(cov_file: str) -> pd.DataFrame:
                        names=['seq_name', 'position', 'coverage'])
 
 
-def calc_read_coverage(ctx: ExecutionContext, aln_file: str) -> pd.DataFrame:
-    """Calculate read coverage per base in reference file."""
+def calc_read_coverage(ctx, aln_file):
     # Naively remove file suffix by splitting on '.' and taking the first entry
     sample_name = os.path.basename(ctx.sample_location[0]).split('.')[0]
     reference_name = os.path.basename(ctx.reference_location).split('.')[0]
